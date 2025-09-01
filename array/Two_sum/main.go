9de86069d5ecc8becb27a798bbb0fe7de9d2181c
MD Waliullah <waliullah.godev@gmail.com>
2025-09-01 07:50:09 +0600

    twoSum

---
+++ array/Two_sum/main.go
@@ -0,0 +1,18 @@
+package main
+
+func twoSum(nums []int, target int) []int { // O(n^2)
+	for i, num := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if num+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	twoSum(nums, target)
+}
